015b5a791e967572fb370b5a5d14cef68671423c
Yani Moore <yanimoore09@gmail.com>
2020-01-22 23:57:24 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,19 @@
-# Use this space to code along with the readme
+def greeting
+  puts "Hello World"
+end
+ 
+greeting
+greeting
+greeting
+greeting
+greeting
+
+def greeting
+puts "hello World"
+end
+
+
+
 
 phrase = "Hello World!"
 puts phrase
